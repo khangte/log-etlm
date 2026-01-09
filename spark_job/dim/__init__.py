d9eea661f10d84675a5fe30209969b308010a4fe
khangte <minhyuk00321@gmail.com>
2026-01-10 08:59:12 +0900

    refactor(dim): status_code 모듈 제거

--- spark_job/dim/__init__.py
+++ spark_job/dim/__init__.py
@@ -2,14 +2,12 @@
 
 from .dim_date import parse_dim_date
 from .dim_service import parse_dim_service
-from .dim_status_code import parse_dim_status_code
 from .dim_time import parse_dim_time
 from .dim_user import parse_dim_user
 
 __all__ = [
     "parse_dim_date",
     "parse_dim_service",
-    "parse_dim_status_code",
     "parse_dim_time",
     "parse_dim_user",
 ]
