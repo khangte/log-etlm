ca1763e517ef8b9ee147f2e2df0eedfa4b23f9e0
khangte <minhyuk00321@gmail.com>
2026-01-11 17:14:05 +0900

    refactor: 프로듀서 패키지 공개 API 추가

---
+++ log_simulator/producer/__init__.py
@@ -0,0 +1,20 @@
+# -----------------------------------------------------------------------------
+# 패키지 : log_simulator/producer
+# 목적   : Kafka Producer 래퍼 및 토픽 매핑
+# -----------------------------------------------------------------------------
+
+from .client import (
+    build_producer_config,
+    close_producer,
+    get_producer,
+    publish_batch_direct,
+)
+from .topic import get_topic
+
+__all__ = [
+    "build_producer_config",
+    "close_producer",
+    "get_producer",
+    "publish_batch_direct",
+    "get_topic",
+]
