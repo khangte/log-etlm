330e63376bc944c73bd0faa884320af7262afa17
khangte <minhyuk00321@gmail.com>
2026-01-14 17:45:57 +0900

    chore: dimension writer 함수 설명 보강

--- spark_job/dimension/writers/dim_writer.py
+++ spark_job/dimension/writers/dim_writer.py
@@ -13,16 +13,21 @@ DIM_USER_TABLE = "analytics.dim_user"
 
 class ClickHouseDimWriter(ClickHouseBatchWriterBase):
     def write_dim_date(self, df: DataFrame):
+        """write_dim_date 처리를 수행한다."""
         self.write_batch(df, DIM_DATE_TABLE, deduplicate_keys=["date"])
 
     def write_dim_time(self, df: DataFrame):
+        """write_dim_time 처리를 수행한다."""
         self.write_batch(df, DIM_TIME_TABLE, deduplicate_keys=["time_key"])
 
     def write_dim_service(self, df: DataFrame):
+        """write_dim_service 처리를 수행한다."""
         self.write_batch(df, DIM_SERVICE_TABLE, deduplicate_keys=["service"])
 
     def write_dim_status(self, df: DataFrame):
+        """write_dim_status 처리를 수행한다."""
         self.write_batch(df, DIM_STATUS_TABLE, deduplicate_keys=["status_code"])
 
     def write_dim_user(self, df: DataFrame):
+        """write_dim_user 처리를 수행한다."""
         self.write_batch(df, DIM_USER_TABLE, deduplicate_keys=["user_id"])
