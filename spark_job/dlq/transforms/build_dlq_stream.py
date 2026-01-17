dae843575ec3f6b6fabdf26c5745bcfd6e2fff0a
khangte <minhyuk00321@gmail.com>
2026-01-17 10:36:26 +0900

    refactor(dlq): DLQ 스트림 변환 정리

--- spark_job/dlq/transforms/build_dlq_stream.py
+++ spark_job/dlq/transforms/build_dlq_stream.py
@@ -6,7 +6,5 @@ from ..parsers.parse_dlq import parse_dlq
 
 
 def build_dlq_stream_df(dlq_source: DataFrame) -> DataFrame:
-    """
-    Kafka logs.dlq 레코드를 DLQ 스트림으로 변환한다.
-    """
+    """DLQ 스트림용 데이터프레임을 생성한다."""
     return parse_dlq(dlq_source)
