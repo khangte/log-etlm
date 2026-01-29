48982c1952b3e1a9b2e4c6dd276ba8f9a7181a54
khangte <minhyuk00321@gmail.com>
2026-01-29 15:14:03 +0900

    chore(clickhouse): grafana_user 집계 테이블 권한 추가

--- infra/clickhouse/sql/90_grants.sql
+++ infra/clickhouse/sql/90_grants.sql
@@ -9,6 +9,7 @@ GRANT SELECT ON analytics.fact_event_lag_1m TO grafana_user;
 GRANT SELECT ON analytics.fact_event_latency_service_1m TO grafana_user;
 GRANT SELECT ON analytics.fact_event_latency_stage_10s TO grafana_user;
 GRANT SELECT ON analytics.fact_event_dlq_agg_1m TO grafana_user;
+GRANT SELECT ON analytics.fact_event_created_stored_1m TO grafana_user;
 GRANT SELECT ON analytics.fact_event_freshness_1m TO grafana_user;
 GRANT SELECT ON analytics.fact_event TO grafana_user;
 GRANT SELECT ON analytics.fact_event_dlq TO grafana_user;
