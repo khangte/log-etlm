9636911b54380d01928b2bdc4164021f13becdd4
khangte <minhyuk00321@gmail.com>
2026-01-31 12:07:40 +0900

    chore(clickhouse): grafana_user 메모리 가드레일 추가

--- infra/clickhouse/sql/90_grants.sql
+++ infra/clickhouse/sql/90_grants.sql
@@ -1,5 +1,10 @@
 -- Grafana read access
 CREATE USER IF NOT EXISTS grafana_user IDENTIFIED BY 'grafana_pwd';
+ALTER USER IF EXISTS grafana_user SETTINGS
+  max_memory_usage = 1073741824,
+  max_bytes_before_external_group_by = 268435456,
+  max_bytes_before_external_sort = 268435456,
+  max_threads = 4;
 
 GRANT SELECT ON analytics.fact_event_agg_1m TO grafana_user;
 GRANT SELECT ON analytics.fact_event_agg_10s TO grafana_user;
